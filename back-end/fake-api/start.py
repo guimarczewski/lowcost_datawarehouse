f6e9746f336ccad03854229cf0e48645fe4f3655
guimarczewski <guimarczewski@gmail.com>
2024-02-29 21:37:05 +0000

    add random data for api

--- back-end/fake-api/start.py
+++ back-end/fake-api/start.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI
+from faker import Faker
+import pandas as pd
+import random
 
 app = FastAPI()
+fake = Faker()
+
+lojapadraoonline = 11
+
+file_name = 'back-end/fake-api/products.csv'
+df = pd.read_csv(file_name, sep=",")
+df['index'] = range(1, len(df) + 1)
+df.set_index('index', inplace=True)
 
 @app.get("/gerar_compra")
 async def gerar_compra():
-    return {
-        "client": "Nome",
-        "creditcard": "Tipo de Cartao",
-        "ean": "Codigo de barra do produto",
-        "price": "Preco do produto",
-        "store": 11,
-        "dateTime": "Data da compra",
-        "clientPosition": "Posicao do cliente"
-    }
+    index = random.randint(1, len(df) - 1)
+    tuple = df.iloc[index]
+    return [
+        {
+            "client": fake.name(),
+            "creditcard": fake.credit_card_provider(),
+            "product": tuple["Product Name"],
+            "ean": int(tuple["EAN"]),
+            "price": round(float(tuple["Price"]) * 1.2, 2), 
+            "clientPosition": fake.location_on_land(),
+            "store": lojapadraoonline,
+            "dateTime": fake.iso8601(),
+        }
+    ]
