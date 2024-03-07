b667c7e82a827b78c3c7767f786bd22085e34b21
guimarczewski <guimarczewski@gmail.com>
2024-03-07 22:34:16 +0000

    upload parquet file to s3

--- backend/datasource/api.py
+++ backend/datasource/api.py
@@ -1,12 +1,15 @@
 import requests
 import pandas as pd
+import pyarrow.parquet as pq
+import datetime
+from io import BytesIO
 from contracts.schema import GenericSchema, CompraSchema
 from typing import List
 
 class APICollector:
-        def __init__ (self, schema):
+        def __init__ (self, schema, aws):
             self._schema = schema
-            self._aws = None
+            self._aws = aws
             self._buffer = None
             return
 
@@ -14,7 +17,16 @@ class APICollector:
             response = self.getData(param)
             response = self.extractData(response)
             response = self.transformDf(response)
-            return response
+            response = self.convertToParquet(response)
+
+            if self._buffer is not None:
+                file_name = self.fileName()
+                print(file_name)
+                # upload para a aws
+                self._aws.upload_file(response, file_name)
+                return True
+
+            return False
 
         def getData(self, param):
             response = None
@@ -42,3 +54,19 @@ class APICollector:
 
         def transformDf(self, response):
             return pd.DataFrame(response)
+
+        def convertToParquet(self, response):
+            self._buffer = BytesIO()
+            try:
+                response.to_parquet(self._buffer)
+                return self._buffer
+            except:
+                print("Erro ao transformar o DF em parquet")
+                self._buffer = None
+
+        def fileName(self):
+            data_atual = datetime.datetime.now().isoformat()
+            # remover as casas decimais do segundo
+            match = data_atual.split(".")
+            
+            return f"api/api-response-compra{match[0]}.parquet"
--- backend/start.py
+++ backend/start.py
@@ -1,8 +1,10 @@
 from datasource.api import APICollector
 from contracts.schema import CompraSchema
+from aws.client import S3Client
 
 schema = CompraSchema
+aws = S3Client()
 
-minha_classe = APICollector(schema).start(3)
+minha_classe = APICollector(schema, aws).start(20)
 
 print(minha_classe)
